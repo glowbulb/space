da612ff6411f010f5022fd8f076c2581275198cf
litebulb <litebulb@dr0p.us>
2017-02-01 20:56:07 -0500

    1

---
+++ build.sh
@@ -0,0 +1,1 @@
+gcc -Wall main.c -lallegro -lallegro_dialog -lallegro_image
--- main.c
+++ main.c
@@ -1,11 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <simple2d.h>
+#include <stdio.h>
+#include "allegro5/allegro.h"
+#include "allegro5/allegro_image.h"
+#include "allegro5/allegro_native_dialog.h"
 
+int main(int argc, char **argv){
 
-int main(int argc, char** argv) {
+   ALLEGRO_DISPLAY *display = NULL;
+   ALLEGRO_BITMAP  *image   = NULL;
 
-  printf("space")
+   if(!al_init()) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize allegro!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
+   }
 
+   if(!al_init_image_addon()) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize al_init_image_addon!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
+   }
+
+   display = al_create_display(800,600);
+
+   if(!display) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to initialize display!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      return 0;
+   }
+
+   image = al_load_bitmap("ship.png");
+
+   if(!image) {
+      al_show_native_message_box(display, "Error", "Error", "Failed to load image!", NULL, ALLEGRO_MESSAGEBOX_ERROR);
+      al_destroy_display(display);
+      return 0;
+   }
+
+   al_draw_bitmap(image,200,200,0);
+
+   al_flip_display();
+   al_rest(100);
+
+   al_destroy_display(display);
+   al_destroy_bitmap(image);
+
+   return 0;
 }
